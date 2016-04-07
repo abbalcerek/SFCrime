7a0e48faebdf78ab4e3b249e9a1096ae7fa48ac0
Adam Balcerek <abalcerek@opi.org.pl>
2016-04-07 14:32:27 +0200

    sadf

--- src/first_model.py
+++ src/first_model.py
@@ -1,45 +1,86 @@
+from matplotlib.cbook import mkdirs
+from sklearn.linear_model.logistic import LogisticRegression
 
+from src.submission import create_submission
 from src.utils import data_path, setup
 import pandas as pd
 from src.longlat import normalize_features, transform_normalized_time
+from src.OneHotTransformer import OneHotTransformer, categorical
+from src.validation import cross_validation
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.externals import joblib
+from sklearn.svm import LinearSVC
 
 
 setup(pd)
-
 # for now only dates, day of week, pd district, resolution, x, y
 
 
-train_path = data_path('train.csv')
-train_frame = pd.read_csv(data_path('train.csv'))
+def transform_set(name, train=True):
+    train_path = data_path(name)
+    train_frame = pd.read_csv(train_path)
+    if train: del train_frame['Descript']
+    del train_frame['Address']
 
-del train_frame['Descript']
-del train_frame['Address']
+    train_frame['X'] = normalize_features(train_frame['X'])
+    train_frame['Y'] = normalize_features(train_frame['Y'])
+    train_frame['Dates'] = train_frame['Dates'].apply(transform_normalized_time)
+    transformer = OneHotTransformer(categorical(train_frame), train_frame.columns)
+    transformer.fit(train_frame)
+    result = transformer.transform_frame(train_frame)
 
-train_frame['X'] = normalize_features(train_frame['X'])
-train_frame['Y'] = normalize_features(train_frame['Y'])
+    not_regex = "^Dates|^PdDistrict|^DayOfWeek|^Resolution|^X|^Y"
+    train_transformed = result.filter(regex=not_regex)
+    label_transformed = None
+    if train: label_transformed = result.filter(regex="^Category")
 
-train_frame['Dates'] = train_frame['Dates'].apply(transform_normalized_time)
+    return train_transformed, label_transformed
 
+# train_path = data_path('train.csv')
+# train_frame = pd.read_csv(data_path('train.csv'))
+#
+# del train_frame['Descript']
+# del train_frame['Address']
+#
+# train_frame['X'] = normalize_features(train_frame['X'])
+# train_frame['Y'] = normalize_features(train_frame['Y'])
+# train_frame['Dates'] = train_frame['Dates'].apply(transform_normalized_time)
+#
+# transformer = OneHotTransformer(categorical(train_frame), train_frame.columns)
+#
+# transformer.fit(train_frame)
+# result = transformer.transform_frame(train_frame)
+#
+# print(result.shape)
+#
+# print(result.dtypes)
+# print("========================================")
+# not_regex = "^Dates|^PdDistrict|^DayOfWeek|^Resolution|^X|^Y"
+# train_transformed = result.filter(regex=not_regex)
+# label_transformed = result.filter(regex="^Category")
 
-from src.OneHotTransformer import OneHotTransformer, categorical
-transformer = OneHotTransformer(categorical(train_frame), train_frame.columns)
-
-transformer.fit(train_frame)
-result = transformer.transform_frame(train_frame)
-
-print(result.dtypes)
-print("========================================")
-not_regex = "^Dates|^PdDistrict|^DayOfWeek|^Resolution|^X|^Y"
-train_transformed = result.filter(regex=not_regex)
-label_transformed = result.filter(regex="^Category")
+train_transformed, label_transformed = transform_set('train.csv')
 
 print(train_transformed.columns)
 print(label_transformed.columns)
 
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import LinearSVC
-
-clf = OneVsRestClassifier(LinearSVC(random_state=0))
+clf = OneVsRestClassifier(LogisticRegression(random_state=0))
 train_prediction = clf.fit(train_transformed, label_transformed).predict(train_transformed)
 
-print(train_prediction.shape)
+# mkdirs(data_path('serialized/model1/'))
+# joblib.dump(clf, data_path('serialized/model1/model1.pkl'))
+
+# print(train_prediction.shape)
+# print(cross_validation(clf, train_transformed, label_transformed))
+
+
+test_transformed, _ = transform_set("test.csv", train=False)
+
+print(test_transformed.columns)
+print(train_transformed.columns)
+
+test_prediction = clf.predict(test_transformed)
+print(test_prediction.shape)
+create_submission(clf.predict(test_transformed), "submission1.csv")
+
+
